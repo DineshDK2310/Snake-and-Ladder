9466c326fb385eb9dc6abd84580a9185c54a32d3
DineshDK2310 <dineshkumardk101199@gmail.com>
2022-01-24 08:49:22 -0800

    Adding:RollDiceUC1 File added successfully

---
+++ RollDiceUC1.java
@@ -0,0 +1,13 @@
+package snakeandladder;
+
+public class RollDiceUC1 {
+
+	public static void main(String[] args) {
+		int startPosition = 0;
+
+		System.out.println("Starting position of Snake and Ladder is : " + startPosition);
+
+
+	}
+
+}
