1cb2defafc02b4cd89fe4b9dcccc582a6448393d
DineshDK2310 <97074518+DineshDK2310@users.noreply.github.com>
2022-01-24 22:23:17 -0800

    Adding:RollDiceUC7 File Added Successfully

--- RollDiceUC7.java
+++ RollDiceUC7.java
@@ -2,42 +2,71 @@ package snakeandladder;
 
 public class RollDiceUC7 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int position=0;
-		int counter=0;
+		int position1=0;
+		int position2=0;
+		int i=2;
 
-		while(position<100)
+		while(position1<100 && position2<100)
 		{
 			double a=Math.floor( ((Math.random()*10) % 6)+1 );
 			double b=Math.floor((Math.random()*10) % 2);
 			int dice=(int)a;
 			int check=(int)b;
-			counter++;
 
-			switch (check) 
+			if(i%2==0)
 			{
+				switch (check) 
+				{
 
-			case 1: 
-				position=position+dice;
-				System.out.println("Ladder");
-				break;
-			case 0: 
+					case 1: 
+						position1=position1+dice;
+						break;
+					case 0: 
+					{
+						position1=position1-dice;
+						if(position1<0)
+							position1=0;
+						break;
+					}
+
+				}
+				if(position1>100)
+					position1=position1-dice;
+				if(position1==100)
+					{
+					System.out.println("Player1 wins");
+					System.out.println("Position= "+position1);
+					break;
+					}
+			}
+			else
 			{
-				position=position-dice;
-				System.out.println("Snake");
-				if(position<0)
-					position=0;
-				break;
-			}
+				switch (check) 
+				{
 
+					case 1: 
+						position2=position2+dice;
+						break;
+					case 0: 
+					{
+						position2=position2-dice;
+						if(position2<0)
+							position2=0;
+						break;
+					}
+
+				}
+				if(position2>100)
+					position2=position2-dice;
+				if(position2==100)
+				{
+				System.out.println("Player2 wins");
+				System.out.println("Position= "+position2);
+				break;
+				}
 			}
-			if(position>100)
-				position=position-dice;
-			System.out.println("Dice number= "+dice);
-			System.out.println("My position: "+position);
-			//System.out.println();
-		}	
-		System.out.println("Total number of times the dice was played= "+counter);
+			i++;
+		}
 
 	}
 
